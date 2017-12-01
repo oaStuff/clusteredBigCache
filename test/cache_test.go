3d9f5bf42641e83186acaa781f20dee3aced0954
prof <omoede@gmail.com>
2017-11-30 20:27:31 -0500

    support for individual element expiration

---
+++ test/cache_test.go
@@ -0,0 +1,59 @@
+package test
+
+import (
+	"testing"
+	"github.com/oaStuff/clusteredBigCache/bigcache"
+	"time"
+	"bytes"
+)
+
+func TestReplacingSameKey(t *testing.T)  {
+	bc,_ := bigcache.NewBigCache(bigcache.DefaultConfig())
+	bc.Set("one", []byte("one"), time.Second * 5)
+	bc.Set("one", []byte("three"), time.Second * 5)
+	val, _ := bc.Get("one")
+	if !bytes.Equal(val, []byte("three")) {
+		t.Error("returned value ought to be equal ot 'three'")
+	}
+}
+
+func TestReplacingSameKeyWithDiffExpiry(t *testing.T)  {
+	bc,_ := bigcache.NewBigCache(bigcache.DefaultConfig())
+	bc.Set("one", []byte("one"), time.Second * 5)
+	bc.Set("one", []byte("three"), time.Second * 3)
+	val, _ := bc.Get("one")
+	if !bytes.Equal(val, []byte("three")) {
+		t.Error("returned value ought to be equal ot 'three'")
+	}
+	time.Sleep(time.Second * 4)
+	val, _ = bc.Get("one")
+	if nil != val {
+		t.Error("value ought to have expired after 3 seconds")
+	}
+}
+
+func TestRemove(t *testing.T)  {
+	bc,_ := bigcache.NewBigCache(bigcache.DefaultConfig())
+	bc.Set("one", []byte("one"), time.Second * 5)
+	bc.Delete("one")
+	val, _ := bc.Get("one")
+	if nil != val {
+		t.Error("value ought not to still be in cache after been deleted")
+	}
+}
+
+func TestExpiry(t *testing.T)  {
+	bc,_ := bigcache.NewBigCache(bigcache.DefaultConfig())
+	bc.Set("one", []byte("one"), time.Second * 5)
+	time.Sleep(time.Second * 3)
+	val, _ := bc.Get("one")
+	if !bytes.Equal(val, []byte("one")) {
+		t.Error("returned value ought to be equal 'one'")
+	}
+	time.Sleep(time.Second * 3)
+	val, _ = bc.Get("one")
+	if nil != val {
+		t.Error("value ought to have expired")
+	}
+}
+
