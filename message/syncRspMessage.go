6a571934a4242fb2810af32cd9ec15b29c3bff5b
omoede.aihe <omoede.aihe@interswitchgroup.com>
2017-12-08 08:49:50 +0100

    implemented sync messages btw nodes

---
+++ message/syncRspMessage.go
@@ -0,0 +1,18 @@
+package message
+
+import "encoding/json"
+
+type SyncRspMessage struct {
+	Code		uint16				`json:"code"`
+	List 		[]ProposedPeer		`json:"list"`
+}
+
+func (sc *SyncRspMessage) Serialize() *NodeWireMessage {
+	data, _ := json.Marshal(sc)
+	return &NodeWireMessage{Code: MsgSyncRsp, Data: data}
+}
+
+func (sc *SyncRspMessage) DeSerialize(msg *NodeWireMessage)  {
+	json.Unmarshal(msg.Data, sc)
+}
+
